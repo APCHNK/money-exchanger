4e7f881fb574acf4a625c941b7eb5277c85cb1b9
Andrii <andriy2003po@gmail.com>
2024-08-24 21:36:28 +0200

    first version which exchange only pln to usd or euro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState(1)
+  const [isResult, setIsResult] = useState(false)
+  const [result, setResult] = useState(0)
+
+  
+  function getCurrencyBid (currency) {
+    fetch(`https://api.nbp.pl/api/exchangerates/rates/c/${currency}/`)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        setResult(() => {
+          let res = 0
+          res = (value / data.rates[0].bid).toFixed(2)
+          if (result) {
+            setIsResult(true)
+          }
+          return res;
+        })
+    })
+  }
+
+  function onChanging (event) {
+    setValue(event.target.value)
+    setIsResult(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="app">
+      <div class="counter"><input onChange={(event) => onChanging(event)} type="number" value={value} /></div>
+      <div class="controls">
+        in
+        <button onClick={(event) => {getCurrencyBid(event.target.getAttribute('data-currency'))}} data-currency={'usd'}>USD</button>
+        <button onClick={(event) => {getCurrencyBid(event.target.getAttribute('data-currency'))}} data-currency={'eur'}>EURO</button>
+      </div>
+      {isResult ? <Result result = {result} /> : null}
     </div>
-  );
+  )
+}
+
+
+const Result = (props) => {
+  return(
+    <>
+      <div className="block">
+        Result is {props.result}
+      </div>
+    </>
+  )
 }
 
 export default App;
